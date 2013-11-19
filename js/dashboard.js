0e1d9e60719d64da6873e595ccbc1e3d044c966e
Tom Caflisch <tomcaflisch@gmail.com>
2013-11-19 15:48:18 -0500

    Fixed rsvp responses not being calculated correctly in the graph

--- js/dashboard.js
+++ js/dashboard.js
@@ -7,16 +7,17 @@ myezteam.controller('DashboardController', ['$scope', '$http', 'myezteamBase', f
 	});
     
 	// Get all of a users upcoming events
-	$scope.getEvents = function(event_id) {
+	$scope.getEvents = function() {
 	
 		$http.get(baseUrl+'v1/events?api_key=9c0ba686-e06c-4a2c-821b-bae2a235fd3d')
 			.success(function(response) {
 		
 				$scope.events = response;
 				
-				// if no event_id is passed in, get the responses for the first event in the list
-			    event_id = event_id == null ? response[0].id : event_id
-				$scope.getResponses(event_id);
+				// if no event_id or event_name is passed in, get the responses for the first event in the list
+			    var event_id = response[0].id;
+			    var event_name = response[0].name;
+				$scope.getResponses(event_id, event_name);
 			})
 			.error(function(response) {
 				$scope.events = 'An error occurred looking for your events. Please try again later.';
@@ -24,14 +25,14 @@ myezteam.controller('DashboardController', ['$scope', '$http', 'myezteamBase', f
 	}
 	
 	// Get all of the responses for a particular event
-	$scope.getResponses = function(event_id) {
+	$scope.getResponses = function(event_id, event_name) {
+	    
+	    console.log(event_name);
 	    
 	    $http.get(baseUrl+'v1/events/' + event_id + '/responses?api_key=9c0ba686-e06c-4a2c-821b-bae2a235fd3d')
 			.success(function(event_responses) {
 				
 				$scope.responses = event_responses;
-				
-				
 				
 				// initially set the no_responses to the total number of responses. We'll change this as we loop through the responses
 				var no_response = $scope.responses.length;  
@@ -41,8 +42,9 @@ myezteam.controller('DashboardController', ['$scope', '$http', 'myezteamBase', f
 				var no = 0;
 				
 				// count the response types (2 yes's, 3 maybe's, etc)
-				for(response in $scope.responses) {
-				    switch (parseInt(response))
+				for(var i = 0; i < $scope.responses.length; i++) {
+				    
+				    switch ($scope.responses[i].response.id)
                     {
                     case 1:
                       // do nothing
@@ -50,6 +52,7 @@ myezteam.controller('DashboardController', ['$scope', '$http', 'myezteamBase', f
                     case 2:
                       yes++;
                       no_response--;
+                      console.log()
                       break;
                     case 3:
                       probably++;
@@ -65,85 +68,81 @@ myezteam.controller('DashboardController', ['$scope', '$http', 'myezteamBase', f
                       break;
                     }
 				}
-
-                	// The rsvp responses data
-	var data = [
-		{
-			name: 'Yes (' + yes + ')',
-			y: yes,
-			url: '#/team/id'
-		},
-		{
-			name: 'Probably (' + probably + ')',
-			y: probably,
-			url: '#/team/id'
-		},
-		{
-			name: 'Maybe (' + maybe + ')',
-			y: maybe,
-			url: '#/team/id'
-		},
-		{
-			name: 'No (' + no + ')',
-			y: no,
-			url: '#/team/id'
-		},
-		{
-			name: 'No Response (' + no_response + ')',
-			y: no_response,
-			url: '#/team/id'
-		}
-	];
-                
-	
-	
-	// Pie chart configs
-	$scope.chartConfig = {
-			chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false
-            },
-            title: {
-                text: 'RSVP Responses for ' + $scope.event
-            },
-            tooltip: {
-        	    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        color: '#000000',
-                        connectorColor: '#000000',
-                        formatter: function() {
-                            return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
-                        }
+                	
+                // The rsvp responses data
+                var data = [
+                    {
+                        name: 'Yes (' + yes + ')',
+                        y: yes,
+                        url: '#/team/id'
+                    },
+                    {
+                        name: 'Probably (' + probably + ')',
+                        y: probably,
+                        url: '#/team/id'
+                    },
+                    {
+                        name: 'Maybe (' + maybe + ')',
+                        y: maybe,
+                        url: '#/team/id'
+                    },
+                    {
+                        name: 'No (' + no + ')',
+                        y: no,
+                        url: '#/team/id'
+                    },
+                    {
+                        name: 'No Response (' + no_response + ')',
+                        y: no_response,
+                        url: '#/team/id'
                     }
-                }
-            },
-            series: [{
-                type: 'pie',
-                name: 'RSVPs',
-                events: {
-            			click: function(e) {
-            				console.log(e.point.options.url);
-            				//location.href = e.point.options.url;
-            			}	
-                	},
-                data: data
-            }]
-	}
+                ];
+                
+                // Pie chart configs
+                $scope.chartConfig = {
+                    chart: {
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false
+                    },
+                    title: {
+                        text: 'RSVP Responses for ' + event_name
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true,
+                                color: '#000000',
+                                connectorColor: '#000000',
+                                formatter: function() {
+                                    return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+                                }
+                            }
+                        }
+                    },
+                    series: [{
+                        type: 'pie',
+                        name: 'RSVPs',
+                        events: {
+                            click: function(e) {
+            				    console.log(e.point.options.url);
+            				    //location.href = e.point.options.url;
+                            }	
+                        },
+                        data: data
+                    }]
+	            }
 			
 			})
 			.error(function(response) {
 				$scope.events = 'An error occurred looking for your events. Please try again later.';
 			});
 	}
-	
-
 	
 	// The colors to be used for the respones in the pie chart
 	var colors = [
@@ -161,42 +160,6 @@ myezteam.controller('DashboardController', ['$scope', '$http', 'myezteamBase', f
 		};
 	});
 
-		
-	// Get all the players of a specific team
-	//$scope.getPlayers = function(team_id, team_name, team_type, team_location, team_description) {
-	
-		// Get all the players of a specific team
-	//	$http.get(baseUrl+'v1/players/team/'+team_id+'?api_key=9c0ba686-e06c-4a2c-821b-bae2a235fd3d')
-	//	.success(function(response) {
-	//		$scope.teamId = team_id;
-	//		$scope.teamName = team_name;
-	//		$scope.teamType = team_type;
-	//		$scope.teamLocation = team_location;
-	//		$scope.teamDescription = team_description;
-	//		$scope['players'] = response;
-	//	})
-	//	.error(function(response) {
-	//		$scope['players'] = 'An error occurred looking for your players. Please try again later.';
-	//	});
-
-	//}
-	
-	// Get all the players of a specific team
-	//$scope.deleteTeam = function(team_id) {
-	
-		// Get all the players of a specific team
-	//	$http.delete(baseUrl+'v1/team/'+team_id+'?api_key=9c0ba686-e06c-4a2c-821b-bae2a235fd3d')
-	//	.success(function(response) {
-	//		
-	//	})
-	//	.error(function(response) {
-	//		$scope['players'] = 'An error occurred looking for your players. Please try again later.';
-	//	});
-
-	//}
-	
-	
-	
     $scope.activateEvent = function(event) {
        $scope.selected = event; 
     };
