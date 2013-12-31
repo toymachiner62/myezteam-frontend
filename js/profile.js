058fd1aa965cafdf4a9acee2ce87c7f844ced5a6
Tom Caflisch <tomcaflisch@gmail.com>
2013-12-30 22:00:58 -0500

    Added error/success messages to profile controller

--- js/profile.js
+++ js/profile.js
@@ -5,17 +5,18 @@ myezteam.controller('ProfileController', ['$scope', '$http', 'myezteamBase', fun
 	myezteamBase.getProfile(function(response) {
 		$scope.profile = response;
 	});
-	//templateFactory.setTitle('My Profile');
 		
 	// Save profile info when the form is submitted
 	$scope.updateProfile = function() {
 		
 		$http.put(baseUrl+'v1/users' + apiKey, $scope.profile)
 			.success(function(response) {
-				//$scope.profile = response;
+			    $scope.error = null;
+				$scope.success = 'Your profile was updated successfully';
 			})
 			.error(function(response) {
-				//$scope.profile = 'An error occurred looking for your info. Please try again later.';
+				$scope.success = null;
+				$scope.error = 'An error occurred trying to update your profile. Please try again later.';
 			});	
 	}
 }]);
