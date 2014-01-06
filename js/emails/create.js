6540b1a1e3e42e1f7be26cdf8b242954681ab6c7
Tom Caflisch <tomcaflisch@gmail.com>
2014-01-06 01:37:34 -0500

    Added form validation (including checkboxes) to create email page and made it so it actually calls the send email method now

--- js/emails/create.js
+++ js/emails/create.js
@@ -6,30 +6,44 @@ myezteam.controller('CreateEventEmailController', ['$scope', '$http', '$routePar
 		$scope.profile = response;
 	});
 	
+	// Set the checkbox values to arrays with null values so that the ng-true-value/ng-false-value work correctly
+    $scope.email = {
+        //player_types: [null, null, null],
+        //response_types: [null, null, null, null, null]
+        player_types: [false, false, false],
+        response_types: [false, false, false, false, false]
+    };
+	
 	// Initially set the time picker time so the display shows something
 	//$scope.event = {start: {time: new Date()}, end: {time: new Date()}};
 	
 	// Get all of a users teams
 	$scope.createEventEmail = function() {
-	
+	console.log($scope.email);
+	console.log($routeParams);
 	    $scope.email.team_id = $routeParams.id  // Set the team id from the url
 	    $scope.email.event_id = $routeParams.event_id // Set the event id from the url
 	    //$scope.event.timezone = "America/Chicago"   // Timezone is always defaulted to Central time for now
 
-	    // Convert player_types and response_types to simple json arrays
+	    // Remove false values from player_types and response_types
 	    var player_types = [];
 	    var response_types = [];
 	    
-	    for(type in $scope.email.player_types) {
-	        player_types[player_types.length] = type;
-	    }
+	    // Loop through all types and convert all the true's to the appropriate integer
+        /*for(var i = 0; i < player_types; i++) {
+            if(player_types[i] == true) {
+                // Set the integer value to 1 + the index
+                $scope.email.player_types.push(i + 1);
+            }
+        }
 	    
 	    for(type in $scope.email.response_types) {
 	        response_types[response_types.length] = type;
-	    }
+	    } */
 	    
-	    $scope.email.player_types = player_types;
-	    $scope.email.response_types = response_types;
+	    // Convert player_types and response_types to integer arrays
+	    $scope.email.player_types = convert_types(player_types);
+	    $scope.email.response_types = convert_types(response_types);
 	    
 	    // Convert $scope.email.include_rsvp to a boolean
 	    if($scope.email.include_rsvp == 'true') {
@@ -77,6 +91,39 @@ myezteam.controller('CreateEventEmailController', ['$scope', '$http', '$routePar
             });
     } 
     
+    /**
+     * Checks if the passed in array (which corresponds to a checkbox group) has at least one box checked.
+     * @param types - An array (player_types or response_types)
+     */
+    $scope.is_required = function(types) {
+
+        for(var i = 0; i < types.length; i++) {
+            if(types[i] == true) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Loop through an array full of booleans convert all the true's to the appropriate integer
+     * @param types - An array full of boolean values to be converted to integers
+     */
+    var convert_types = function(types) {
+        
+        var converted_types = [];
+        
+        for(var i = 0; i < types; i++) {
+            if(types[i] == true) {
+                // Set the integer value to 1 + the index
+                converted_types.push(i + 1);
+            }
+        }
+        
+        return converted_types;
+    }
+    
     // Watches $scope.email.default for changes. If the value changes, convert it to a boolean datatype and set some other variables
     $scope.$watch('email.default', function() {
         
@@ -92,18 +139,37 @@ myezteam.controller('CreateEventEmailController', ['$scope', '$http', '$routePar
         }
    });
    
-    // Watches $scope.email.include_rsvp for changes. If the value changes, convert it to a boolean datatype
-    /*$scope.$watch('email.include_rsvp', function() {
+    // Watches $scope.email.response_types for changes. If the value changes, make sure to remove all 
+    //      the "null" values from the array os the array returned only contains integers which correspond 
+    //      to the response_types in the db
+   /* $scope.$watch('email.response_types', function() {
         
         // If $scope.email exists
         if(typeof $scope.email !== 'undefined') {
-            if($scope.email.include_rsvp == 'true') {
-                $scope.email.include_rsvp = true;
-            } else if($scope.email.include_rsvp == 'false') {
-                $scope.email.include_rsvp = false;
+            for(var i = 0; i < $scope.email.response_types.length; i++) {
+                console.log('type = '+$scope.email.response_types[i]);
+                if($scope.email.response_types[i] == 'null') {
+                    $scope.email.response_types[i] = null;
+                }
             }
         }
-   });*/
+   });
+   
+   // Watches $scope.email.player_types for changes. If the value changes, make sure to remove all 
+    //      the "null" values from the array os the array returned only contains integers which correspond 
+    //      to the player_types in the db
+    $scope.$watch('email.player_types', function() {
+        
+        // If $scope.email exists
+        if(typeof $scope.email !== 'undefined') {
+            for(var i = 0; i < $scope.email.player_types.length; i++) {
+                console.log('type = '+$scope.email.player_types[i]);
+                if($scope.email.player_types[i] == 'null') {
+                    $scope.email.player_types[i] = null;
+                }
+            }
+        }
+   }); */
 		
     // Initially get the team name for the breadcrumbs
 	$scope.getTeam();
