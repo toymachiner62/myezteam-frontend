52ef6997944d59659b32c3d78a8401970dc3f400
Tom Caflisch <tomcaflisch@gmail.com>
2014-10-23 11:34:03 -0500

    Fixed email rsvp

--- js/app.js
+++ js/app.js
@@ -167,14 +167,14 @@ myezteamLogin.run(['$location', '$rootScope', function($location, $rootScope) {
 		}
 	});
 
-	// If a user has previously asked to be remembered, set the session storage token and redirect
-	if(localStorage.getItem("myezteamToken") != null) {
-		sessionStorage.setItem('myezteamToken', localStorage.getItem('myezteamToken'));
-		window.location.href = "main.html";
-	}
-
 	// If a user clicked any link, except an email rsvp link
 	if(!$location.path().startsWith('/responses/email_rsvp/')) {
+		
+		// If a user has previously asked to be remembered, set the session storage token and redirect
+		if(localStorage.getItem("myezteamToken") != null) {
+			sessionStorage.setItem('myezteamToken', localStorage.getItem('myezteamToken'));
+			window.location.href = "main.html";
+		}
 
   	// If a user is logged in (if a sessionStorage token exists), redirect the to the main page.
   	if(sessionStorage.getItem('myezteamToken') != null) {
