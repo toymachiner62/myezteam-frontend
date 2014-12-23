2d4a3c0fabf8e9e451cafe5e2aa61f36fd6a7c86
Tom Caflisch <tomcaflisch@gmail.com>
2014-12-22 19:38:47 -0600

    Streamlined creating new events

--- js/events/create.js
+++ js/events/create.js
@@ -6,14 +6,19 @@ myezteam.controller('CreateEventController', ['$scope', '$http', '$routeParams',
 		myezteamBase.getProfile(function(response) {
 			$scope.profile = response;
 		});
+		
+		/**
+		 * Call on page load
+		 */
+		var init = function() {
+			setupEvent();
+			$scope.getTeam();	// Initially get the team name for the breadcrumbs
+		};
 
 		/**
-		 * Get all of a users teams
+		 * Creates a new event
 		 */
 		$scope.createEvent = function() {
-
-			$scope.event.team_id = $routeParams.id // Set the team id from the url
-			$scope.event.timezone = "America/Chicago" // Timezone is always defaulted to Central time for now
 
 			// Convert dates/times to correct format the api call expects
 			var start_date = new Date($scope.event.start.date);
@@ -29,7 +34,8 @@ myezteam.controller('CreateEventController', ['$scope', '$http', '$routeParams',
 				.success(function(response) {
 					$scope.error = null;
 					$scope.success = 'Event ' + $scope.event.name + ' created successfully!';
-					$scope.event = null; // clear form fields
+					$scope.event = {}; // clear form fields
+					setupEvent();
 					$("html, body").animate({
 						scrollTop: 0
 					}, "slow"); // scroll to top of page so success/error message is visible
@@ -42,6 +48,48 @@ myezteam.controller('CreateEventController', ['$scope', '$http', '$routeParams',
 					}, "slow"); // scroll to top of page so success/error message is visible
 				});
 		}
+		
+		/**
+		 * When the start date changes, set the end date to the same
+		 */
+		$scope.$watch('event.start.date', function(val) {
+			if(val) {
+				$scope.event.end.date = val;
+			}
+		});
+		
+		/**
+		 * When the start time changes, set the end time to 1 hour later
+		 */
+		$scope.$watch('event.start.time', function(newVal, oldVal) {
+			
+			// If newVal exists and is not the same as oldVal....basically don't fire on page load
+			if (newVal && newVal !== oldVal) {
+				var endTime = new Date(newVal);
+				endTime.setHours(newVal.getHours() + 1);
+				$scope.event.end.time = endTime;
+			}
+		});
+		
+		
+		/**
+		 * Set some of the initial event attributes
+		 */
+		var setupEvent = function() {
+			
+			$scope.event = {
+				team_id: $routeParams.id, // Set the team id from the url
+				timezone: "America/Chicago", // Timezone is always defaulted to Central time for now
+				start: {
+					date: new Date(),
+					time: getRoundedTime(false)
+				},
+				end: {
+					date: new Date(),
+					time: getRoundedTime(true)
+				}
+			};
+		};
 
 		/**
 		 * Gets the team information for the team we're creating the event for
@@ -64,13 +112,13 @@ myezteam.controller('CreateEventController', ['$scope', '$http', '$routeParams',
 		}
 
 		/**
-		 * Creates a new Date object with the time rounded to the nearest hour
+		 * Creates a new Date object with the time initially set to 7pm
 		 *
 		 * @param end   - A boolean that determines if we should add an hour to the date or not
 		 */
 		var getRoundedTime = function(end) {
 			var date = new Date();
-			date.setHours(date.getHours() + Math.round(date.getMinutes() / 60));
+			date.setHours(19);	// Set the time to 7pm
 			if (end) {
 				date.setHours(date.getHours() + 1);
 			}
@@ -78,17 +126,6 @@ myezteam.controller('CreateEventController', ['$scope', '$http', '$routeParams',
 			return date;
 		}
 
-		// Initially set the time picker time so the display shows something
-		$scope.event = {
-			start: {
-				time: getRoundedTime(false)
-			},
-			end: {
-				time: getRoundedTime(true)
-			}
-		};
-		// Initially get the team name for the breadcrumbs
-		$scope.getTeam();
-
+		init();	// Call on page load
 	}
 ]);
